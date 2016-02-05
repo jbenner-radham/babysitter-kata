5c2d53497d1cd8311549135c6d64e14061ddd9a9
James Benner <james.benner@gmail.com>
2016-02-05 09:36:27 -0500

    First working "Babysitter" test.

---
+++ test/test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let assert     = require('assert')
+let Babysitter = require('../src/Babysitter')
+
+describe('Babysitter', () => {
+    let times      = { start: 18.01, bed: 20.00, end: 3.50 }
+    let babysitter = new Babysitter(times);
+
+    it('Should be paid for full hours (no fractional hours).', () => {
+        assert.equal(babysitter.start, 18.00)
+        assert.equal(babysitter.bed, 20.00)
+        assert.equal(babysitter.end, 4.00)
+    })
+})
