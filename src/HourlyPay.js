0c9174c847d0be637686ac08caeca21196fe3613
James Benner <james.benner@gmail.com>
2016-02-05 09:54:22 -0500

    Added working "Should be paid $12/hour from start-time to bedtime" test.

---
+++ src/HourlyPay.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/**
+ * The babysitter
+ * - starts no earlier than 5:00PM (17:00)
+ * - leaves no later than 4:00AM (04:00)
+ * - gets paid $12/hour from start-time to bedtime
+ * - gets paid $8/hour from bedtime to midnight
+ * - gets paid $16/hour from midnight to end of job
+ * - gets paid for full hours (no fractional hours)
+ */
+module.exports = {
+    /** @type {number} */
+    startTimeToBedtimeWage: 12
+}
--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@
 
 let assert     = require('assert')
 let Babysitter = require('../src/Babysitter')
+let HourlyPay  = require('../src/HourlyPay')
 
 describe('Babysitter', () => {
     let times      = { start: 18.01, bed: 20.00, end: 3.50 }
@@ -20,5 +21,11 @@ describe('Babysitter', () => {
         assert.equal(babysitter.start, 18.00)
         assert.equal(babysitter.bed, 20.00)
         assert.equal(babysitter.end, 4.00)
+    })
+})
+
+describe('Hourly Pay', () => {
+    it('Should be paid $12/hour from start-time to bedtime.', () => {
+         assert.equal(HourlyPay.startTimeToBedtimeWage, 12)
     })
 })
