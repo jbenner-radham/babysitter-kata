e1651c188ef694d82308d557e9e0ad4703e25dde
James Benner <james.benner@gmail.com>
2016-02-05 13:50:31 -0500

    Updated CLI UX to display times and tagged release.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // Modules
 let Babysitter = require('./src/Babysitter')
 let HourlyPay  = require('./src/HourlyPay')
+let sprintf    = require('sprintf-js').sprintf
 
 // Vars
 let times      = require('./times.json')
@@ -21,4 +22,8 @@ for (let hour of hours) {
     }
 }
 
-console.info(`Total Pay: $${pay}`)
+console.info('Start-time: ' + sprintf('%.2f', times.start))
+console.info('Bedtime:    ' + sprintf('%.2f', times.bed))
+console.info('End of Job: ' + sprintf('%2.2f', times.end))
+console.info('-----------------')
+console.info(`Total Pay:  $${pay}`)
